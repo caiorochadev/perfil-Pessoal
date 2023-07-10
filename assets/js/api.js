af23b1d8876b35ae549b708ac719cdc52bffd63c
caiorochadev <139146069+caiorochadev@users.noreply.github.com>
2023-07-10 13:30:17 -0300

    mudança de url

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/caiorochadev/perfil-Pessoal/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/caiorochadev/API-CV-CaioRocha/main/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
